e73894ec396329ced74857006411d6fbc1982827
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-08-12 22:44:48 -0700

    completed part1 16-22

---
+++ Part01/AverageOfThreeNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AverageOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+        System.out.println("The average is " + ((double) (first + second + third) / 3));
+    }
+}
