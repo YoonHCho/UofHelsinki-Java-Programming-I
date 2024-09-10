d27cbbb92c24935f038093cf0e6187ebd1267c4b
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-09-09 20:27:37 -0700

    completed part02 21-26

---
+++ Part02/Reprint.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Reprint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many times?");
+        int num = scanner.nextInt();
+        for (int i = 0; i < num; i++) {
+            printText();
+        }
+    }
+
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
