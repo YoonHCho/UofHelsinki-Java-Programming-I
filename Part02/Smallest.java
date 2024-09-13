3b3bd4afd441e7369a3422320ba9dd2551afa360
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-09-12 21:56:53 -0700

    completed part02 27-30

---
+++ Part02/Smallest.java
@@ -0,0 +1,9 @@
+public class Smallest {
+    public static void main(String[] args) {
+        System.out.println("Smallest: " + smallest(2, 7));
+    }
+
+    public static int smallest(int num1, int num2) {
+        return Math.min(num1, num2);
+    }
+}
