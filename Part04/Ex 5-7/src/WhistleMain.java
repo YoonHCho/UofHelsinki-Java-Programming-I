e2aeee2bb2310f231e230459512e0b40b6c1bfe8
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2025-01-29 23:02:32 -0800

    completed part04 5-7

---
+++ Part04/Ex 5-7/src/WhistleMain.java
@@ -0,0 +1,10 @@
+public class WhistleMain {
+    public static void main(String[] args) {
+        Whistle duckWhistle = new Whistle("Kvaak");
+        Whistle roosterWhistle = new Whistle("Peef");
+
+        duckWhistle.sound();
+        roosterWhistle.sound();
+        duckWhistle.sound();
+    }
+}
